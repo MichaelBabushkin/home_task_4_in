7e533d6fc64807b9798dffec553890a6f8c012c2
michael <mishaba1990@gmail.com>
2022-03-26 23:55:35 +0300

    added grid insted of table

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -6,6 +6,8 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+
 
 import useStyles from "./Style";
 
@@ -18,6 +20,33 @@ type RankRow = {
   date:string,
 }
 
+
+const columns: GridColDef[] = [
+  { field: 'Rank', headerName: 'Rank', width: 90 },
+  {
+    field: 'Nickname',
+    headerName: 'Nickname',
+    sortable: false,
+    width: 150,
+  },
+  {
+    field: 'Highscore(moves)',
+    headerName: 'Highscore(moves)',
+    width: 150,
+  },
+  {
+    field: 'Time',
+    headerName: 'Time',
+    sortable: false,
+    width: 110,
+  },
+  {
+    field: 'Date',
+    headerName: 'Date',
+    sortable: false,
+    width: 160,
+  },
+];
 
 
 
@@ -41,32 +70,43 @@ function Leaderboard(){
   
 
     return (
-      <TableContainer component={Paper}>
-      <Table className={classes.leaderboardTable} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell >Rank</TableCell>
-            <TableCell align="right">Nickname</TableCell>
-            <TableCell align="right">Highscore(moves)</TableCell>
-            <TableCell align="right">Time</TableCell>
-            <TableCell align="right">Date</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {highscoreData.map((row,index) => (
-            <TableRow key={index+1}>
-              <TableCell component="th" scope="row">
-                {index+1}
-              </TableCell>
-              <TableCell align="right">{row.nickname}</TableCell>
-              <TableCell align="right">{row.moves}</TableCell>
-              <TableCell align="right">{row.time}</TableCell>
-              <TableCell align="right">{row.date}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    //   <TableContainer component={Paper}>
+    //   <Table className={classes.leaderboardTable} aria-label="simple table">
+    //     <TableHead>
+    //       <TableRow>
+    //         <TableCell >Rank</TableCell>
+    //         <TableCell align="right">Nickname</TableCell>
+    //         <TableCell align="right">Highscore(moves)</TableCell>
+    //         <TableCell align="right">Time</TableCell>
+    //         <TableCell align="right">Date</TableCell>
+    //       </TableRow>
+    //     </TableHead>
+    //     <TableBody>
+    //       {highscoreData.map((row,index) => (
+    //         <TableRow key={index+1}>
+    //           <TableCell component="th" scope="row">
+    //             {index+1}
+    //           </TableCell>
+    //           <TableCell align="right">{row.nickname}</TableCell>
+    //           <TableCell align="right">{row.moves}</TableCell>
+    //           <TableCell align="right">{row.time}</TableCell>
+    //           <TableCell align="right">{row.date}</TableCell>
+    //         </TableRow>
+    //       ))}
+    //     </TableBody>
+    //   </Table>
+    // </TableContainer>
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={highscoreData}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+        disableSelectionOnClick
+      />
+    </div>
+
       );
 }
 
