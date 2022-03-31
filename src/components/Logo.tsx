505e7bd994287cbe9075476fedd517b91596c980
michael <mishaba1990@gmail.com>
2022-03-31 09:58:12 +0300

    logo path

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -7,7 +7,7 @@ export default function Logo({ }: Props) {
     const classes = useStyles();
     return (
         <div className={classes.logo}>
-            <img src="http://localhost:3001/assets/zoominfo-logo-vector.svg"></img>
+            <img src="http://localhost:3000/assets/zoominfo-logo-vector.svg"></img>
         </div>
     )
 }
